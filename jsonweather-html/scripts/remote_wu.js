4fd9548711412b78e74d5bb21e18c86f4aaec347
dglinzey <dglinzey@gmail.com>
2016-11-25 14:12:49 -0500

    update

--- jsonweather-html/scripts/remote_wu.js
+++ jsonweather-html/scripts/remote_wu.js
@@ -25,14 +25,18 @@ $(function () {
       $.ajax({
           url : "//api.wunderground.com/api/5fabe86224c61a86/geolookup/conditions/q/" + lat + "," + long + ".json",
           dataType : "jsonp",
-          success : function(parsed_json) {
-              console.log(parsed_json);
-              var location = parsed_json['location']['city'];
-              var tempF = parsed_json['current_observation']['temp_f'];
-              var feelsLike = parsed_json['current_observation']['feelslike_f'];
-              var weather = parsed_json['current_observation']['weather'];
-              var windDescription = parsed_json['current_observation']['wind_string'];
-              $("#currentTemp").text(tempF + " F");
+          success : function(data) {
+              console.log(data);
+              var city = data['location']['city'];
+              var state = data['location']['state'];
+              var tempF = data['current_observation']['temp_f'];
+              var feelsLike = data['current_observation']['feelslike_f'];
+              var weather = data['current_observation']['weather'];
+              var windDescription = data['current_observation']['wind_string'];
+              var RtempF = Math.round(tempF);
+              $("#cityDisplay").text(city + ", " + state);
+              $('.title').prepend(city + ", " + state + " ");
+              $("#currentTemp").text(RtempF + " F");
               $("#summary").text(weather);
               $("#feelsLike").text("Feels Like: " + feelsLike + " F");
               $("#windDescription").text(windDescription);
