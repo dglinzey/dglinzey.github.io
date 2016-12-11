7476bc0642b34d2997392e5436bff34d5ce45d7b
dglinzey <dglinzey@gmail.com>
2016-12-10 17:05:15 -0700

    update

---
+++ modules/debtmodules/modules.js
@@ -0,0 +1,5 @@
+$(function() {
+    $(".foot").load("/modules/debtmodules/footer.html");
+    $(".header").load("/modules/debtmodules/header.html");
+    $(".nav").load("/modules/debtmodules/nav.html");
+});
